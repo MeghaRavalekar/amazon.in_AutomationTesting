f303110cfcbaf387508faf196653a591fe02a01a
LENOVO S145 <LENOVO S145@Megha>
2023-10-28 21:11:50 +0530

    Initial Commit

---
+++ src/test/java/testCases/TC1_Login.java
@@ -0,0 +1,30 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import baseClass.BaseClass;
+import pageFactory.LoginPage;
+import pageFactory.ProductPage;
+
+public class TC1_Login extends BaseClass {
+
+	@Test
+	public void Login() throws InterruptedException {
+
+		LoginPage Login_Ob = new LoginPage(driver);
+
+		// Hover on Account And List text
+		// click on Sign in button
+		Login_Ob.ClickSignin();
+
+		// Enter username and password, click on sign in Btn
+         Login_Ob.Login("9699445909", "Test@1234");
+
+		// Verify user is loggedin on not
+		Login_Ob.VerifyUserLogin();
+
+		// Logout
+		Login_Ob.Signout();
+	}
+
+}
---
+++ src/test/java/testCases/TC3_PlaceOrder.java
@@ -0,0 +1,42 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import baseClass.BaseClass;
+import pageFactory.LoginPage;
+import pageFactory.ProductPage;
+
+public class TC3_PlaceOrder extends BaseClass {
+	@Test
+	public void PlaceOrder() throws InterruptedException {
+		LoginPage Login_Ob = new LoginPage(driver);
+
+		ProductPage ProductPage_Ob = new ProductPage(driver);
+
+		// Hover on Account And List text
+		// click on Sign in button
+		Login_Ob.ClickSignin();
+
+		// Enter username and password, click on sign in Btn
+		Login_Ob.Login("9699445909", "Test@1234");
+		
+		// click on header cart button
+		ProductPage_Ob.OpenHeaderCart();
+
+		// check cart items
+		ProductPage_Ob.VerifyCartIetms();
+
+		// Search Product
+		ProductPage_Ob.SearchProduct("Mobile");
+
+		// Click on Prod Name
+		ProductPage_Ob.SelectProduct();
+
+		// Go to Prod Detail Page
+		Login_Ob.RedirectToProdDetailPage();
+
+		// Place Order
+		ProductPage_Ob.PlaceOrder();
+	}
+
+}
