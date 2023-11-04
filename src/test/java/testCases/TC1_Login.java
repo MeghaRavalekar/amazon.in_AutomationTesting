cc73ad97c97ff7567d55c5e7299d9f69c5881c7e
MeghaRavalekar <141602636+MeghaRavalekar@users.noreply.github.com>
2023-11-04 15:04:30 +0530

    Update TC1_Login.java

--- src/test/java/testCases/TC1_Login.java
+++ src/test/java/testCases/TC1_Login.java
@@ -1,30 +1,20 @@
 package testCases;
-
 import org.testng.annotations.Test;
-
 import baseClass.BaseClass;
 import pageFactory.LoginPage;
 import pageFactory.ProductPage;
 
 public class TC1_Login extends BaseClass {
-
-	@Test
+        @Test
 	public void Login() throws InterruptedException {
-
-		LoginPage Login_Ob = new LoginPage(driver);
-
+                LoginPage Login_Ob = new LoginPage(driver);
 		// Hover on Account And List text
 		// click on Sign in button
 		Login_Ob.ClickSignin();
-
 		// Enter username and password, click on sign in Btn
-         Login_Ob.Login("9699445909", "Test@1234");
-
+                Login_Ob.Login("9699445909", "Test@1234");
 		// Verify user is loggedin on not
 		Login_Ob.VerifyUserLogin();
-
-		// Logout
+                // Logout
 		Login_Ob.Signout();
-	}
-
-}
+	}}
